861e1517f3ff27e4ec9f87ff02363bd81ea095d1
Dinith21 <dinmee.21@gmail.com>
2024-09-27 15:41:15 +1000

    Initial Commit

---
+++ Murdle/MurdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurdleApp.swift
+//  Murdle
+//
+//  Created by Dinith Meegahapola on 27/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct MurdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
