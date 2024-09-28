c995ad15270fa49204eff525a3c133fc3e9af56c
Dinith21 <dinmee.21@gmail.com>
2024-09-28 16:22:37 +1000

    addded functional working grid view

---
+++ Murdle/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Murdle
+//
+//  Created by Dinith Meegahapola on 27/9/2024.
+//
+
+import Foundation
---
+++ Murdle/WorkingGridView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkingGridView.swift
+//  Murdle
+//
+//  Created by Dinith Meegahapola on 27/9/2024.
+//
+
+import SwiftUI
+
+struct WorkingGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkingGridView()
+}
